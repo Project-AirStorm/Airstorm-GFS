4f21f56d541143230190069ebae64ed093d2c298
joshmfrancis <Thejoshuafrancis@gmail.com>
2024-12-11 16:50:08 -0600

    Formatting small fix

--- backend/app.py
+++ backend/app.py
@@ -2,17 +2,19 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) #enables cross origin resource sharing AKA "CORS"
+CORS(app)  # enables cross origin resource sharing AKA "CORS"
+
 
 @app.route('/api/test', methods=['GET'])
 def greet():
     return jsonify({"message": "Hello Airstorm Team, lets do it!"})
 
+
 @app.route('/home')
 def home():
-    #this will eventually contain our Graphcast Data, served up to react
+    # this will eventually contain our Graphcast Data, served up to react
     return jsonify({"message": "Hi, I am the flask API Everyone!"})
-   
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
