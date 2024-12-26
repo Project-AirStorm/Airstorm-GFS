57cea4d70ad702db6d4158ca3f549ca54080dff6
joshmfrancis <Thejoshuafrancis@gmail.com>
2024-12-26 02:40:02 -0600

    fixed location centering

--- frontend/src/pages/Weather.js
+++ frontend/src/pages/Weather.js
@@ -9,7 +9,7 @@ const Weather = () => {
     iframe.width = '100%';
     iframe.height = '100%';
     iframe.style.border = 'none';
-    iframe.src = `https://api.maptiler.com/maps/6fc667a0-09bd-4b69-bd77-1ce5af52e91b/?key=${process.env.REACT_APP_MAPTILER_API_KEY}#-0.2/0.00000/141.02679`;
+    iframe.src = `https://api.maptiler.com/maps/6fc667a0-09bd-4b69-bd77-1ce5af52e91b/?key=${process.env.REACT_APP_MAPTILER_API_KEY}#3.5/37.51718/-94.68554`;
 
     if (mapContainer.current) {
       mapContainer.current.appendChild(iframe);
