e5a644b547744a18d59932ae46fc7fb1e5e7c983
Connor <zittrauerconnor@gmail.com>
2025-02-10 15:41:46 -0600

    added comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,11 +31,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Public sign-in routes */}
+        {/* Public sign-in routes that are only available to users
+            that have not been authenticated by Clerk yet  */}
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<Signup />} />
 
-        {/* Root route - redirect based on auth state */}
+        {/* These are our "fallback routes." If The user tries to 
+            Type in anything after projectairstorm.com/asdf, this 
+            redirects the React router to the /login page. */}
         <Route
           path="/"
           element={
@@ -50,13 +53,17 @@ function App() {
           }
         />
 
-        {/* Protected application routes */}
+        {/* Protected application routes! These are only accessible Clerk login has been 
+            authenticated and the user is signed in. 
+            Our entire application is wrapped in the <Layout> Component. */}
         <Route
           element={
             <>
               <SignedIn>
                 <Layout>
                   <Routes>
+                    {/* Dynamically generates all proctected application routes from 
+                        the ROUTES are in Routes.js */}
                     {Object.values(ROUTES).map(({ path, element: Element }) => (
                       <Route key={path} path={path} element={<Element />} />
                     ))}
