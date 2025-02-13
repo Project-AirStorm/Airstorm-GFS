80326243b156c27c9187c08c32983d6268fe2dbd
Connor <zittrauerconnor@gmail.com>
2025-02-12 21:37:56 -0600

    fixed typo

--- backend/src/db/test-sql-queries.sql
+++ backend/src/db/test-sql-queries.sql
@@ -1,4 +1,4 @@
--- View all locations associatied with every user
+-- Retrieves all locations associatied with every user
 SELECT L.*, U.username
 FROM Locations L
 JOIN Users U ON L.user_id = U.user_id;
